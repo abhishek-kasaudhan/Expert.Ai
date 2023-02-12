03274608fad3cebc2390173714f7c93906b0745c
Abhishek Kasaudhan <124250453+abhishek-kasaudhan@users.noreply.github.com>
2023-02-13 02:21:29 +0530

    Delete my-app/out directory

--- my-app/out/_next/static/chunks/pages/news-a9e6e3e3af204c02.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[134],{5115:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/news",function(){return u(9236)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=5115)}),_N_E=n.O()}]);
